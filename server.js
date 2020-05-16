8e4d684868b6b3fca50924fd5b41cf6ad2cdc7e4
shantanusohni <60313951+shantanusohni@users.noreply.github.com>
2020-05-16 18:23:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults({static:'./build'})
  
 server.use(middlewares)
 server.use(router)
-server.listen(80, () => {
+server.listen(3000, () => {
   console.log('JSON Server is running')
 })
