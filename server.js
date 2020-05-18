2d4a60959ebc879b5a33b2e2d6cd67306749430f
shantanusohni <60313951+shantanusohni@users.noreply.github.com>
2020-05-18 16:25:22 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data.json')
-const middlewares = jsonServer.defaults({static:'./usr/share/nginx/html/react-ui/build'})
+const middlewares = jsonServer.defaults({static:'./usr/share/nginx/html/build'})
  
 server.use(middlewares)
 server.use(router)
