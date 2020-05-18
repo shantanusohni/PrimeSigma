2d87112a6035f2a28feadf60f372634d7ca1d17e
shantanusohni <60313951+shantanusohni@users.noreply.github.com>
2020-05-18 16:16:20 +0530

    update

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data.json')
-const middlewares = jsonServer.defaults({static:'./react-ui/build'})
+const middlewares = jsonServer.defaults({static:'./usr/share/nginx/html/react-ui/build'})
  
 server.use(middlewares)
 server.use(router)
-server.listen(80, () => {
+server.listen(3000, () => {
   console.log('JSON Server is running')
 })
